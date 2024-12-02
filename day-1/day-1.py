f59cda1bae43e370e306ad026c41dd09a1311f21
David Shaw <dshaw11@utk.edu>
2024-12-02 09:50:56 -0500

    implemented a quick sort of the lists

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -6,7 +6,36 @@ def main():
 
     f = open("advent-1-input.txt", "r")
     for x in f:
-        print(x.split())
-        
+        line = x.split()
+        a.append(int(line[0]))
+        b.append(int(line[1]))
+
+    quicksort(a)
+    quicksort(b)
+    print(len(b))
+
+
+
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i+1], array[high] = array[high], array[i+1]
+    return i+1
+
+def quicksort(array, low=0, high=None):
+    if high is None:
+        high = len(array) - 1
+
+    if low < high:
+        pivot_index = partition(array, low, high)
+        quicksort(array, low, pivot_index-1)
+        quicksort(array, pivot_index+1, high)
+
 
 main()
